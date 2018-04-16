e01f7e72def7e1f20e8c33e6cc975f3687cb1171
= <lamonteh1@gmail.com>
2018-04-16 11:59:55 -0500

    Added generic to the get method to get rid of duplication

--- monobuild.nim
+++ monobuild.nim
@@ -3,30 +3,23 @@ import tables
 import strutils
 import flowsettings
 
-proc get(settings: SettingsTable, key: string): string =
+proc get[T](settings: SettingsTable, key: string): T =
   if settings.hasKey(key):
     let val = settings[key]
-    if val.kind == sString:
-      return val.strVal
-    else:
-      return ""
-  return ""
+    
+    if (T is string) and val.kind == sString:
+      return cast[T](val.strVal)
+    elif (T is seq[string]) and val.kind == sSeqString:
+      return cast[T](val.seqVal)
 
-proc getSeq(settings: SettingsTable, key: string): seq[string] =
-  if settings.hasKey(key):
-    let val = settings[key]
-    if val.kind == sSeqString:
-      return val.seqVal
-    else:
-      return @[]
-  return @[]
+  raise newException(IndexError, "Invalid Settings Key")
 
 proc build_command(runtime: string, settings: SettingsTable): string =
 
-  let mode = get(settings, "mode")
-  let dotnet = get(settings, "dotnet")
-  let build_folder = get(settings, "build_folder")
-  let self_contained = get(settings, "self_contained")
+  let mode = get[string](settings, "mode")
+  let dotnet = get[string](settings, "dotnet")
+  let build_folder = get[string](settings, "build_folder")
+  let self_contained = get[string](settings, "self_contained")
 
   let c_folder = if not isNilOrEmpty(build_folder): build_folder & "/" & mode & "/" & runtime else: ""
   let current_folder = if not isNilOrEmpty(c_folder): "-o " & c_folder & " " & self_contained else: "" & self_contained
@@ -34,11 +27,11 @@ proc build_command(runtime: string, settings: SettingsTable): string =
   return dotnet & " publish -c " & mode & " -r " & runtime & " " & current_folder
 
 proc build_default(settings: SettingsTable) =
-  let runtime = get(settings, "runtime")
+  let runtime = get[string](settings, "runtime")
   discard execShellCmd(build_command(runtime, settings))
 
 proc build_all(settings: SettingsTable) =
-  let runtimes = getSeq(settings, "runtimes")
+  let runtimes = get[seq[string]](settings, "runtimes")
   for runtime in runtimes:
     discard execShellCmd(build_command(runtime, settings))
 
